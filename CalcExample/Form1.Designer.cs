f20155195b543e59928d22b4a2951d4f381c0316
Jsphere <baydo.justin@gmail.com>
2012-06-10 13:42:11 -0700

    Removed Label

--- CalcExample/Form1.Designer.cs
+++ CalcExample/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.grpBoxStatus = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // grpBoxStatus
@@ -41,28 +40,17 @@
             this.grpBoxStatus.TabIndex = 0;
             this.grpBoxStatus.TabStop = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "C# Calculator";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(119, 177);
             this.ControlBox = false;
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.grpBoxStatus);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Calculator";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -70,7 +58,6 @@
         #endregion
 
         private System.Windows.Forms.GroupBox grpBoxStatus;
-        private System.Windows.Forms.Label label1;
 
     }
 }
